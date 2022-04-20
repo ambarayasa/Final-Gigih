7f1e593b269405f84e005b3799a0880cb9e58d65
ambarayasa <ign.ambara@gmail.com>
2022-04-20 17:58:06 +0800

    Modify menus controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,13 +1,30 @@
 class MenusController < ApplicationController
   def index
+    @menus = Menu.all
   end
 
   def new
+    @menu = Menu.new
+  end
+
+  def create
+    menu = Menu.create(params.require(:menu).permit(:name, :description, :price))
+
+    redirect_to menus_path
   end
 
   def edit
+    @menu = Menu.find_by(id: params[:id])
+  end
+
+  def update
+    @menu = Menu.find_by(id: params[:id])
+
+    @menu.update(params.require(:menu).permit(:name, :description, :price))
+    redirect_to menus_path
   end
 
   def show
+    @menu = Menu.find_by(id: params[:id])
   end
 end
