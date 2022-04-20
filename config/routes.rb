97d22460182bb15ebdae6187b7fe2b730e32c2a2
ambarayasa <ign.ambara@gmail.com>
2022-04-20 15:22:51 +0800

    Create controller for menus

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'menus/index'
+  # get 'menus/new'
+  # get 'menus/edit'
+  # get 'menus/show'
+  resources :menus
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
